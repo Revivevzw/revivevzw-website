7d4903d33c22c14fdef3ab10224e1b4759716469
Maarten Brysbaert <maarten.brysbaert@qite.be>
2021-04-10 21:00:37 +0200

    fix for news-bar

--- Revivevzw.Frontend/src/app/components/news-bar/news-bar.component.ts
+++ Revivevzw.Frontend/src/app/components/news-bar/news-bar.component.ts
@@ -1,49 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, style, animate } from "@angular/animations";
 import { NewsItemApiService } from 'src/app/services/news-item-api.service';
 import { NewsItem } from 'src/app/models/news-item.model';
 import { Localization } from 'src/app/models/localization.model';
 import { LocalizeService } from 'src/app/services/localize.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news-bar',
   templateUrl: './news-bar.component.html',
-  styleUrls: ['./news-bar.component.scss'],
-  animations: [
-    trigger('newsBarAnimation', [
-      transition('void => *', [
-        style({ marginLeft: '100vw' }),
-        animate('500ms ease-in-out', style({ marginLeft: 0 }))
-      ]),
-      transition('* => void', [
-        animate('500ms ease-in-out', style({ marginLeft: '-100vw' }))
-      ])
-    ])
-  ]
+  styleUrls: ['./news-bar.component.scss']
 })
 export class NewsBarComponent implements OnInit {
 
   constructor(
     private newsItemApiService: NewsItemApiService,
     private localizeService: LocalizeService,
-    private router: Router
   ) { }
 
   public newsItems: Array<NewsItem> = [];
-
-  public currentItem = 0;
+  public isBusy: boolean = true;
 
   ngOnInit(): void {
-    this.startAnimation();
     this.SetNewsItems();
-  }
-
-  public redirect = () => {
-    const url = this.newsItems[this.currentItem].url
-    if(!url) return;
-    // this.router.navigate([url]);
-    window.open(url, 'blank');
   }
 
   public localize = (localization: Localization) => {
@@ -51,18 +28,10 @@ export class NewsBarComponent implements OnInit {
   }
 
   private SetNewsItems = () => {
+    this.isBusy = true;
     this.newsItemApiService.getAll().subscribe(newsItems => {
       this.newsItems = newsItems;
+      this.isBusy = false;
     })
-  }
-
-  private startAnimation = () => {
-    setInterval(() => {
-      if (this.newsItems.length - 1 <= this.currentItem) {
-        this.currentItem = 0;
-      } else {
-        this.currentItem += 1;
-      }
-    }, 6000)
   }
 }
