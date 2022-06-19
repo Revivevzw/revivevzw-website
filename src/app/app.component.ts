ce74567b3630d354343ce89df4508509b8d18241
Maarten Brysbaert <maarten.brysbaert@qite.be>
2022-06-19 11:52:57 +0200

    Add background color of navigation when scrolled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,20 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } fr
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'revive-vzw';
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() { }
 
-  @ViewChild('newsBar', { read: ElementRef }) public newsBar: ElementRef<any>;
   @ViewChild('navigation', { read: ElementRef }) public navigation: ElementRef<any>;
 
   ngAfterViewInit() {
-    // window.addEventListener('scroll', this.scrolled);
+    window.addEventListener('scroll', this.scrolled);
   }
 
   private scrolled = () => {
-    let newsBarBoundingClientRect = this.newsBar.nativeElement.getBoundingClientRect();
-    if (newsBarBoundingClientRect.height + newsBarBoundingClientRect.y <= 0) {
-      this.renderer.setStyle(this.navigation.nativeElement, 'position', 'fixed');
+    if (window.scrollY > 0) {
       this.renderer.addClass(this.navigation.nativeElement.children[0], 'header--scrolled');
     } else {
-      this.renderer.setStyle(this.navigation.nativeElement, 'position', 'static');
       this.renderer.removeClass(this.navigation.nativeElement.children[0], 'header--scrolled');
     }
   }
