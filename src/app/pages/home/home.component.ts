8bae7afce32d4dcb34f7883fe794cfdd12efb9db
Maarten Brysbaert <maarten.brysbaert@qite.be>
2023-11-19 16:41:56 +0100

    Take expdate into account

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -42,7 +42,7 @@ export class HomeComponent implements OnInit {
         })
     }
 
-    public $carrouselItems = this.carrouselApi.getCarrouselItems();
+    public $carrouselItems = this.carrouselApi.getCarrouselItems().pipe(map(x => x.filter(y => new Date(y.expDate) > new Date())));
     public $carrouselImages = this.$carrouselItems.pipe(map(x => x.filter(y => !y.isYoutubeId)));
     public $carrouselYoutubeUrls = this.$carrouselItems.pipe(map(x => x.filter(y => y.isYoutubeId).map(y => this.buildYoutubeUrl(y.url))));
 
