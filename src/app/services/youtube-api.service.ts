01831ee846d87bff56582893e187738eda437694
Maarten Brysbaert <maarten.brysbaert@qite.be>
2023-11-19 16:09:27 +0100

    Commented out unused youtube-api-service

--- src/app/services/youtube-api.service.ts
+++ src/app/services/youtube-api.service.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { environment } from 'src/environments/environment';
 
-@Injectable({
-    providedIn: 'root'
-})
-export class YoutubeApiService {
+// @Injectable({
+//     providedIn: 'root'
+// })
+// export class YoutubeApiService {
 
-    public url = 'https://www.googleapis.com/youtube/v3/';
+//     public url = 'https://www.googleapis.com/youtube/v3/';
 
-    constructor(private http: HttpClient) { }
+//     constructor(private http: HttpClient) { }
 
-    public getDetails(id: string) {
-        const path = `${this.url}search?key=${environment.youtubeApiKey}&id=${id}`
-        return this.http.get(path);
-    }
-}
+//     public getDetails(id: string) {
+//         const path = `${this.url}search?key=${environment.youtubeApiKey}&id=${id}`
+//         return this.http.get(path);
+//     }
+// }
